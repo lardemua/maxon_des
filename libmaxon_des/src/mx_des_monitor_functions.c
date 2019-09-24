f84c85574b59363bbd9b4eeb4c92519af81dfc3b
Bernardo Lourenço <bernardo.lourenco@ua.pt>
2019-09-24 13:57:50 +0100

    added record functions

--- libmaxon_des/src/mx_des_monitor_functions.c
+++ libmaxon_des/src/mx_des_monitor_functions.c
@@ -1,2 +1,36 @@
 #include "mx_des.h"
 #include "mx_des_functions.h"
+
+des_error des_read_velocity(des_context *context, bool type, des_reading *reading)
+{
+    assert(context != NULL);
+    assert(reading != NULL);
+
+    __SEND_FRAME(0x28, 1, type & 0x1)
+
+    des_frame frame;
+
+    __RECEIVE_FRAME(frame)
+    __VALIDATE_FRAME(frame, 2)
+
+    memcpy(reading, frame.data, 4);
+
+    return DES_OK;
+}
+
+des_error des_read_current(des_context *context, bool type, des_reading *reading)
+{
+    assert(context != NULL);
+    assert(reading != NULL);
+
+    __SEND_FRAME(0x28, 1, type & 0x1)
+
+    des_frame frame;
+
+    __RECEIVE_FRAME(frame)
+    __VALIDATE_FRAME(frame, 2)
+
+    memcpy(reading, frame.data, 4);
+
+    return DES_OK;
+}
